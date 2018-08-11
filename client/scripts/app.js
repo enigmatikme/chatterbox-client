27c05ecf4dff73e5c67329ee7a6480c651645d9b
enigmatikme <khsu0720@gmail.com>
2018-08-11 16:58:23 -0700

    Working on rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,47 +19,77 @@ var app = {
   // this.roomname= '4chan'
 };
 
-app.init = function() {
+app.rooms = [];
+
+app.init = function () {
+
   console.log(document.location.search);
 };
 
-app.send = function(message) {
+app.send = function (message) {
+  message.text = `<script>$('body').css('background-image', url("https://vignette.wikia.nocookie.net/harrypotter/images/1/1a/Kenneth_Branagh_as_Gilderoy-Lockhart_%286%29.jpg/revision/latest?cb=20100123194408"))</script>`
   $.ajax({
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
-    contentType: 'application/json'
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
 };
-app.fetch = function() {
+app.fetch = function (room) {
+
+  room = room || ''
+  console.log(room);
   $.ajax({
-    // url: 'http://parse.sfm8.hackreactor.com/',
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?order=-createdAt',
     type: 'GET',
+    contentType: 'application/json',
+    data: `where={"roomname": "${room}"}`,
+    // data: 'where={"roomname": "lobby"}',
+    success: function (data) {
+      console.log('chatterbox: Message received', data);
+      for (var i = 0; i < data['results'].length; i++) {
+        app.renderMessage(data['results'][i]);
+        if (!app.rooms.includes(data['results'][i]['roomname'])) {
+          app.rooms.push(data['results'][i]['roomname']);
+        }
+      }
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
 };
-app.clearMessages = function() {
+app.clearMessages = function () {
   var node = document.getElementById('chats');
   node.innerHTML = '';
-  // console.log(node);
 };
 
-app.renderMessage = function(message) {
-  var node = document.createElement('div');
-  node.username = message.username;
-  node.src = message;
-  // console.log(node);
-  document.getElementById('chats').appendChild(node);
-  // console.log($("chats"))
+app.renderMessage = function (message) {
+
+  newdiv2 = document.createElement("div");
+  existingdiv1 = document.getElementById('chats');
+
+  $("#chats").append($(`<div class="msg ${message.roomname}" data-user=${message.username}><b>${message.username}</b>:<br>${message.text}</div>`))
+
+
 };
 
-app.renderRoom = function(room) {
+app.renderRoom = function (room) {
+  console.log(room);
   var node = document.createElement('div');
   node.src = room;
   document.getElementById('roomSelect').append(node);
 };
 
-app.handleUsernameClick = function() {
-  $('#chats').on('click', function() {
-      
+app.handleUsernameClick = function () {
+  $('#chats').on('click', function () {
+
 
   });
 };
@@ -69,18 +99,24 @@ var message = {
   text: 'trololo',
   roomname: '4chan'
 };
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
 
+
+$(document).ready(function () {
+  app.init();
+  var node = document.getElementById('chatbox');
+  app.fetch();
+  $(".button").on("click", function () {
+    var userText = $('.inputtext').val();
+
+    var username = window.location.search.slice(10);
+    $('#chats').append($(`<div class="msg" data-user=${username}><b>${username}</b><br>${userText}</div>`)) //.text(username));
+    var messageObj = { username: username, text: userText, roomname: 'lobby' }
+    app.send(messageObj);
+
+  })
+  $('.rooms').on('change', function () {
+    var roomname = $('.rooms').val();
+    app.fetch(roomname);
+    console.log(roomname);
+  });
+});
